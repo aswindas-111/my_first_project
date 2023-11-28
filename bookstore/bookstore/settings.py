4b733a135f7618e0ba74c60acc5286c3707c315c
ASWINDAS <cvaswin1997@gmail.com>
2023-11-28 10:50:05 +0530

    media file name changed in settings

--- bookstore/bookstore/settings.py
+++ bookstore/bookstore/settings.py
@@ -156,7 +156,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'assets/')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
-MEDIA_URL = '/media/'
+MEDIA_URL = '/assets/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'assets')
 
 # Default primary key field type
